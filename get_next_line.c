9573602a52e205a8ce80f9584b5038cf1928c0e0
Eric LAZO <erlazo@e3r7p23.42.fr>
2019-03-11 19:29:21 +0100

    final stretch

--- get_next_line.c
+++ get_next_line.c
@@ -6,99 +6,79 @@
 /*   By: erlazo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/17 11:00:08 by erlazo            #+#    #+#             */
-/*   Updated: 2019/03/08 17:30:01 by erlazo           ###   ########.fr       */
+/*   Updated: 2019/03/11 19:28:02 by erlazo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>		// delete this line
-
 #include "get_next_line.h"
 
-static int		gnl(char **line, char **save, int fd)
+static int		term(t_glst **lst, t_glst *elem)
 {
-	char	buff[BUFF_SIZE + 1];
-	int		len;
-	char	*tmp;
+	t_glst	*tmp;
 
-//	printf("test1\n");
-	tmp = NULL;
-	len = ft_findchar(*save, DELIM);
-	
-//	printf("pos: %i ", len);
-//	printf("start save: %s|\n", *save);
-
-	if (len != -1)		//!= -1 meaning there is a \n somewhere...
+	tmp = *lst;
+	if (tmp->fd == elem->fd)
 	{
-//		printf("cut\n");
+		*lst = tmp->next;
+		free(tmp);
+	}
+	else
+	{
+		while (tmp->next->fd != elem->fd)
+			tmp = tmp->next;
+		tmp->next = tmp->next->next;
+		free(elem);
+	}
+	return (0);
+}
 
-		if (!(*line = ft_strsub(*save, 0, len)))
-			return (-1);
-		if (!(tmp = ft_strsub(*save, len + 1, ft_strlen(*save) - len - 1)))	// i think this might be the problem...
-			return (-1);
-		ft_strclr(*save);
-		free(*save);
-		*save = tmp;
+static int		gnl(char **l, char **s, t_glst **lst, t_glst *elem)
+{
+	int		i;
+	char	*p;
+	char	b[BUFF_SIZE + 1];
 
-//		printf("line: %s\n", *line);
-//		printf("cut save: %s|\n", *save);
+	p = NULL;
+	if ((i = ft_findchar(*s, DELIM)) != -1)
+	{
+		if (!(*l = ft_strsub(*s, 0, i++)) ||
+			(!(p = ft_strsub(*s, i, ft_strlen(*s) - i))))
+			return (-1);
+		free(*s);
+		*s = p;
 		return (1);
 	}
-	ft_bzero(buff, BUFF_SIZE + 1);
-	if ((len = read(fd, buff, BUFF_SIZE)) < 0)
+	ft_bzero(b, BUFF_SIZE + 1);
+	if ((i = read(elem->fd, b, BUFF_SIZE)) < 0 ||
+		(i > 0 && !(p = ft_strjoin(*s, b))) ||
+		(*s[0] && ++i == 1 && !(p = ft_strjoin(*s, "\n"))))
 		return (-1);
-	if (len > 0)	//something was read.
-	{
-//		printf("save: %s\n", *save);
-		
-		if (!(tmp = ft_strjoin(*save, buff)))
-			return (-1);
-//		printf("test2\n");
-		free(*save);
-		*save = tmp;
-//		printf("save: %s\n", *save);
-	}
-/*	else if (len == 0 && *save[0] != '\0' && ft_strlen(*save) > 0 && *save[ft_strlen(*save) - 1] != '\n')		// not the cause of doubling up problem but still needs help
-	{
-		len = 1;
-		if (!(tmp = ft_strjoin(*save, "\n")))
-			return (-1);
-		free(*save);
-		*save = tmp;
-	}
-*/	if (len > 0) // something was read. or it's the end of the folder and we had to add an n
-		return (gnl(line, save, fd));
-
-	free(*save);		// i guess...
-	return (0); // i think....
+	free(*s);
+	*s = p;
+	return ((i > 0) ? gnl(l, s, lst, elem) : term(lst, elem));
 }
 
-int		get_next_line(const int fd, char **line) // need basic checks prolly
+int				get_next_line(const int fd, char **line)
 {
-	static char	*tab[1000000];				// it's static, should be fine when prog ends, no need for free...
-	int			ret;
+	static t_glst	*lst = NULL;
+	t_glst			*new_elem;
+	t_glst			*tmp;
 
-//	*line = ft_strnew(0);		// ok so this didn't fix as much as i would have liked...
-	if (fd < 0 || !line || BUFF_SIZE <= 0)
+	tmp = lst;
+	if (fd < 0 || !line || BUFF_SIZE < 1)
 		return (-1);
-	if (!tab[fd] && !(tab[fd] = ft_strnew(1)))
+	while (tmp)
+	{
+		if (tmp->fd == fd)
+			return (gnl(line, &tmp->s, &lst, tmp));
+		tmp = tmp->next;
+	}
+	if (!(new_elem = (t_glst*)malloc(sizeof(t_glst))))
 		return (-1);
-
-//	printf("test1\n");
-//	if (!tab[fd])		// no dif with below version
-//		tab[fd] = ft_strnew(0);
-//	if (!tab[fd] && !(tab[fd] = ft_strnew(0)))	// strnew of 0 ... very interesting
-//		return (-1);
-	
-	ret = gnl(line, &tab[fd], fd);
-	
-//	if (ret == 0)
-//		free(tab[fd]);
-	return (ret);
+	new_elem->fd = fd;
+	new_elem->s = ft_strnew(1);
+	new_elem->next = lst;
+	lst = new_elem;
+	new_elem->start = lst;
+	return (gnl(line, &new_elem->s, &lst, new_elem));
 }
-
-
-
-
-
-
-
