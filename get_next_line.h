fc0f74ee03d4b876b82a5e8da127142f28de21ef
Eric LAZO <erlazo@e3r5p17.42.fr>
2019-03-12 19:33:48 +0100

    test apparently

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,15 @@
 /*   By: erlazo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/22 14:55:59 by erlazo            #+#    #+#             */
-/*   Updated: 2019/03/10 19:32:33 by erlazo           ###   ########.fr       */
+/*   Updated: 2019/03/11 19:27:58 by erlazo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 1		//right spot???
-# define DELIM '\n'			//do i need the ' ' ???`
+# define BUFF_SIZE 7
+# define DELIM '\n'
 
 # include "libft.h"
 # include <unistd.h>
@@ -27,7 +27,6 @@ typedef struct	s_glst
 	struct s_glst	*next;
 	struct s_glst	*start;
 }				t_glst;
-
 int				get_next_line(const int fd, char **line);
 
 #endif
