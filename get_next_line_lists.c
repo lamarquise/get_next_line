ad994e140a7586f71bffb1eae0e129aa70bdd8aa
Eric LAZO <erlazo@e3r5p17.42.fr>
2019-03-12 20:18:02 +0100

    should be everything i need

--- get_next_line_lists.c
+++ get_next_line_lists.c
@@ -6,68 +6,71 @@
 /*   By: erlazo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/17 11:00:08 by erlazo            #+#    #+#             */
-/*   Updated: 2019/03/10 19:32:27 by erlazo           ###   ########.fr       */
+/*   Updated: 2019/03/11 18:45:57 by erlazo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
+#include "get_next_line.h"
 
 
 	// need to test if my list actually deletes, does it free and all that... are there any leaks ???
 	// that should be it tho.
 
 
+// the possible terminate function
 
 
-#include "get_next_line.h"
+
+static int		term(t_glst **lst, t_glst *elem)
+{
+	t_glst	*tmp;
+
+	tmp = *lst;
+	if (tmp->fd == elem->fd)
+	{
+		*lst = tmp->next;
+		free(tmp);
+	}
+	else
+	{
+		while (tmp->next->fd != elem->fd)
+			tmp = tmp->next;
+		tmp->next = tmp->next->next;
+		free(elem);
+	}
+	return (0);
+}
 
 static int		gnl(char **l, char **s, int fd, t_glst **lst, t_glst *elem)	// keep fd?
 {
 	int		i;
 	char	*p;
 	char	b[BUFF_SIZE + 1];
-	t_glst	*tmp;
 
 	p = NULL;
 	if ((i = ft_findchar(*s, DELIM)) != -1)
 	{
-		if (!(*l = ft_strsub(*s, 0, i++)) || (!(p = ft_strsub(*s, i, ft_strlen(*s) - i))))
+		if (!(*l = ft_strsub(*s, 0, i++)) ||
+			(!(p = ft_strsub(*s, i, ft_strlen(*s) - i))))
 			return (-1);
 		free(*s);
 		*s = p;
 		return (1);
 	}
 	ft_bzero(b, BUFF_SIZE + 1);
-	if ((i = read(fd, b, BUFF_SIZE)) < 0 || (i > 0 && !(p = ft_strjoin(*s, b))) ||
+	if ((i = read(fd, b, BUFF_SIZE)) < 0 ||
+		(i > 0 && !(p = ft_strjoin(*s, b))) ||
 		(*s[0] && ++i == 1 && !(p = ft_strjoin(*s, "\n"))))
 		return (-1);
 	free(*s);
 	*s = p;
-	if (i == 0 && *s == NULL)		// this needs to be thoroughly tested.
-	{
-		tmp = *lst;
-		if (tmp->fd == fd)
-		{
-			*lst = tmp->next;
-			free(tmp);
-		}
-		else
-		{
-			while (tmp->next->fd != fd)
-				tmp = tmp->next;
-			tmp->next = tmp->next->next;
-			free(elem);
-		}
-	}
-	return ((i > 0) ? gnl(l, s, fd, lst, elem) : 0);			// if i were to add another func i would call it here instead of 0 and have it return 0
-}																// it would terminate the list elem in question.
+//	if (i == 0 && *s == NULL)
+	return ((i > 0) ? gnl(l, s, fd, lst, elem) : term(lst, elem));
+}
 
-
-
-
-
-int		get_next_line(const int fd, char **line)		// 
+int		get_next_line(const int fd, char **line)
 {
 	static t_glst	*lst = NULL;
 	t_glst			*new_elem;
@@ -82,7 +85,7 @@ int		get_next_line(const int fd, char **line)		//
 			return (gnl(line, &tmp->s, fd, &lst, tmp));
 		tmp = tmp->next;
 	}
-	if (!(new_elem = (t_glst*)malloc(sizeof(t_glst))))		// can't really use the lib since its for lsts not glsts....
+	if (!(new_elem = (t_glst*)malloc(sizeof(t_glst))))
 		return (-1);
 	new_elem->fd = fd;
 	new_elem->s = ft_strnew(1);
@@ -91,16 +94,3 @@ int		get_next_line(const int fd, char **line)		//
 	new_elem->start = lst;
 	return (gnl(line, &new_elem->s, fd, &lst, new_elem));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ get_next_line_mess.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erlazo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/17 11:00:08 by erlazo            #+#    #+#             */
+/*   Updated: 2019/03/11 18:43:37 by erlazo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>		// delete this line
+
+#include "get_next_line.h"
+
+static int		gnl(char **line, char **save, int fd)
+{
+	char	buff[BUFF_SIZE + 1];
+	int		len;
+	char	*tmp;
+
+//	printf("test1\n");
+	tmp = NULL;
+	len = ft_findchar(*save, DELIM);
+	
+//	printf("pos: %i ", len);
+//	printf("start save: %s|\n", *save);
+
+	if (len != -1)		//!= -1 meaning there is a \n somewhere...
+	{
+//		printf("cut\n");
+
+		if (!(*line = ft_strsub(*save, 0, len)))
+			return (-1);
+		if (!(tmp = ft_strsub(*save, len + 1, ft_strlen(*save) - len - 1)))	// i think this might be the problem...
+			return (-1);
+		ft_strclr(*save);
+		free(*save);
+		*save = tmp;
+
+//		printf("line: %s\n", *line);
+//		printf("cut save: %s|\n", *save);
+		return (1);
+	}
+	ft_bzero(buff, BUFF_SIZE + 1);
+	if ((len = read(fd, buff, BUFF_SIZE)) < 0)
+		return (-1);
+	if (len > 0)	//something was read.
+	{
+//		printf("save: %s\n", *save);
+		
+		if (!(tmp = ft_strjoin(*save, buff)))
+			return (-1);
+//		printf("test2\n");
+		free(*save);
+		*save = tmp;
+//		printf("save: %s\n", *save);
+	}
+/*	else if (len == 0 && *save[0] != '\0' && ft_strlen(*save) > 0 && *save[ft_strlen(*save) - 1] != '\n')		// not the cause of doubling up problem but still needs help
+	{
+		len = 1;
+		if (!(tmp = ft_strjoin(*save, "\n")))
+			return (-1);
+		free(*save);
+		*save = tmp;
+	}
+*/	if (len > 0) // something was read. or it's the end of the folder and we had to add an n
+		return (gnl(line, save, fd));
+
+	free(*save);		// i guess...
+	return (0); // i think....
+}
+
+int		get_next_line(const int fd, char **line) // need basic checks prolly
+{
+	static char	*tab[1000000];				// it's static, should be fine when prog ends, no need for free...
+	int			ret;
+
+//	*line = ft_strnew(0);		// ok so this didn't fix as much as i would have liked...
+	if (fd < 0 || !line || BUFF_SIZE <= 0)
+		return (-1);
+	if (!tab[fd] && !(tab[fd] = ft_strnew(1)))
+		return (-1);
+
+//	printf("test1\n");
+//	if (!tab[fd])		// no dif with below version
+//		tab[fd] = ft_strnew(0);
+//	if (!tab[fd] && !(tab[fd] = ft_strnew(0)))	// strnew of 0 ... very interesting
+//		return (-1);
+	
+	ret = gnl(line, &tab[fd], fd);
+	
+//	if (ret == 0)
+//		free(tab[fd]);
+	return (ret);
+}
+
+
+
+
+
+
+
