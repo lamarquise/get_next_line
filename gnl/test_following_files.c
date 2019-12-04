635f781530d5ed3bd0daca9872c6d93b8b7d5edc
Eric LAZO <erlazo@e1r8p8.42.fr>
2019-12-04 21:35:01 +0100

    ok now i got everyting

---
+++ gnl/test_following_files.c
@@ -0,0 +1,63 @@
+
+
+#include "get_next_line_bonus.h"
+
+//#include "get_next_line.h"
+
+
+#include <fcntl.h>
+#include <stdio.h>
+
+void    ft_putendl_fd(char const *s, int fd)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+		++i;
+	write(fd, s, i);
+	write(fd, "\n", 1);
+}
+
+
+int	main(int ac, char **av)
+{
+	if (ac == 1) { ft_putendl_fd("no input file given", 1); return (0); }
+	
+	char *line;
+	int ret;
+	int t;
+	int fd;
+	int i = 1;
+
+	while (av[i])
+	{
+		printf("\n[[[[[ %s ]]]]]\n\n", av[i]);
+		fd = open(av[i], O_RDONLY);
+		ret = 1;
+		t = 0;
+		while ((ret = get_next_line(fd, &line)))
+		{
+			printf("[%d] %s\n", ret, line);
+			free(line);
+			if (ret == -1)
+				return (0);
+			t += ret;
+		}
+		printf("[%d] %s\n", ret, line);
+			free(line);
+			if (ret == -1)
+				return (0);
+			t += ret;
+	
+		printf("=== Count === %d\n", t);
+	
+		printf("=== Rappel apres avoir atteint la fin de fichier ===\n");
+		ret = get_next_line(fd, &line);
+		printf("[%d] %s\n", ret, line);
+		free(line);
+
+		close(fd);
+		i++;
+	}
+}
