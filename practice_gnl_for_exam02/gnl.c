c2d2bbdb87a2cbf7b32fbbc8a475f2fa149d431a
Me <me@Philip.local>
2021-12-08 08:57:56 +0100

    setting up folder for practicing for exam02

---
+++ practice_gnl_for_exam02/gnl.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gnl.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: me <erlazo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/08 08:56:33 by me                #+#    #+#             */
+/*   Updated: 2021/12/08 08:57:36 by me               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// figure out which includes
+// pretty sure i'm allowed to use write
+// there is no norm in exam
+// will try it recursively but IDK maybe iterative is best?
